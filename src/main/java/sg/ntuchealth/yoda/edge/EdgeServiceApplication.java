1048c194cfb76d15cf4779cf34ff8e58a4866b43
Dinesh <dineshchand.geetharavichandran@ntucenterprise.sg>
2021-08-14 12:39:06 +0800

    merged changes from dev

--- src/main/java/sg/ntuchealth/yoda/edge/EdgeServiceApplication.java
+++ src/main/java/sg/ntuchealth/yoda/edge/EdgeServiceApplication.java
@@ -1,5 +1,7 @@
 package sg.ntuchealth.yoda.edge;
 
+import java.util.Arrays;
+import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -8,12 +10,16 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
@@ -52,6 +58,22 @@ public class EdgeServiceApplication {
   @LoadBalanced
   public WebClient.Builder loadBalancedWebClientBuilder() {
     return WebClient.builder();
+  }
+
+  @Profile({"alpha", "uat"})
+  @Bean
+  public CorsWebFilter corsWebFilter() {
+
+    final CorsConfiguration corsConfig = new CorsConfiguration();
+    corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+    corsConfig.setMaxAge(3600L);
+    corsConfig.setAllowedMethods(Arrays.asList("*"));
+    corsConfig.addAllowedHeader("*");
+
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfig);
+
+    return new CorsWebFilter(source);
   }
 
   @Bean

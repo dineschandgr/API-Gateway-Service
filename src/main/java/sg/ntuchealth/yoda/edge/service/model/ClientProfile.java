1cb33221af200a76734a91d06524ccaf03557267
Dineshchand Geetha Ravichandran <dineshchand.geetharavichandran@ntucenterprise.sg>
2021-09-28 05:51:42 +0000

    Merged in feature/remove-unused-fields (pull request #36)
    removing the un-used fields from LinkID response
    
    * removing the un-used fields from LinkID response
    
    
    Approved-by: Shruti Madan

--- src/main/java/sg/ntuchealth/yoda/edge/service/model/ClientProfile.java
+++ src/main/java/sg/ntuchealth/yoda/edge/service/model/ClientProfile.java
@@ -1,13 +1,11 @@
 package sg.ntuchealth.yoda.edge.service.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.util.Map;
 
 @Data
 @Builder
@@ -27,24 +25,4 @@ public class ClientProfile {
 
   @NotNull(message = "Email cannot be null")
   private String email;
-
-  private String phone_number_last_verified;
-
-  private String email_last_verified;
-
-  private String date_of_birth;
-
-  private String preferences;
-
-  private String memberships;
-
-  private String myinfo_last_verified;
-
-  private String created_at;
-
-  private String updated_at;
-
-  Map<String, Object> association;
-
-  Map<String, Object> metadata;
 }
